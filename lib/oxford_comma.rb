de5031b9000615ba39c7ad9339877b7984bc1d30
Anthony Lepore <violacellonyc@gmail.com>
2020-07-29 14:35:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,27 @@
 def oxford_comma(array)
+  new_array = []
+  the_last = array.last
+  if array.size == 1 
+    return array.join 
+     elsif array.size == 2 
+     new_array = array
+     new_array[0].concat(" and ")
+   else
+  array.each do |change|
+    if change == the_last
+      temp = "and "
+     temp.concat(change)
+     change = temp
+      new_array.push(change)
+    else 
+      change.concat(", ")
+      
+      new_array.push(change)
+    end
+ 
+  end
+end
+
+   array = new_array.join
 
 end
