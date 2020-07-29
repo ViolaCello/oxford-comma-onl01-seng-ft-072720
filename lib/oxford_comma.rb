6f7583891dc37ba536235ec88f4027623b8449be
Anthony Lepore <violacellonyc@gmail.com>
2020-07-29 14:18:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,11 @@
 def oxford_comma(array)
+  new_array = []
   array.each do |change|
     if change == array.last end
     else 
-      
+      new_array.push(change + ", ")
+    end
+    array = new_array
   
 
 
